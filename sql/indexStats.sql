ff88a1f6d20091b9a8d7b0b2f6640ce04c9f0a9c
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-09-25 11:47:18 -0500

    cleaned up formatting

--- sql/indexStats.sql
+++ sql/indexStats.sql
@@ -3,21 +3,14 @@
 
 --Index Usuage Stats
 
-SELECT t.tablename                                                         AS
-       "relation", 
+SELECT t.tablename, 
        indexname, 
-       c.reltuples                                                         AS
-       num_rows, 
-       Pg_size_pretty(Pg_relation_size(Quote_ident(t.tablename) :: text))  AS
-       table_size, 
-       Pg_size_pretty(Pg_relation_size(Quote_ident(indexrelname) :: text)) AS
-       index_size, 
-       idx_scan                                                            AS
-       number_of_scans, 
-       idx_tup_read                                                        AS
-       tuples_read, 
-       idx_tup_fetch                                                       AS
-       tuples_fetched 
+       c.reltuples AS num_rows, 
+       Pg_size_pretty(Pg_relation_size(Quote_ident(t.tablename) :: text))  AS table_size, 
+       Pg_size_pretty(Pg_relation_size(Quote_ident(indexrelname) :: text)) AS index_size, 
+       idx_scan AS number_of_scans, 
+       idx_tup_read AS tuples_read, 
+       idx_tup_fetch AS tuples_fetched 
 FROM   pg_tables t 
        left outer join pg_class c 
                     ON t.tablename = c.relname 
@@ -37,6 +30,6 @@ FROM   pg_tables t
                                join pg_stat_all_indexes psai 
                                  ON x.indexrelid = psai.indexrelid) AS foo 
                     ON t.tablename = foo.ctablename 
-WHERE  t.schemaname = 'public'
-ORDER  BY 1, 2
+WHERE t.schemaname = 'public'
+ORDER BY tablename, indexname
 \g (format=html)
