7fe9527ba2eb1c7560833f2ed1c11bcead875eaa
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-09-26 09:02:13 -0500

    Drop IP and port

--- sql/version.sql
+++ sql/version.sql
@@ -1,8 +1,4 @@
 \o 'html/version.html'
 --Postgres version
-SELECT Version()          AS "Postgres Version", 
-        Inet_server_addr() AS "Server IP", 
-       setting            AS "Port Number"
-FROM   pg_settings 
-WHERE  name = 'port'
+SELECT Version() AS "Postgres Version"
 \g (format=html)
