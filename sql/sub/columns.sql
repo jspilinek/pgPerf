0c62db9033fb768d3d87bc9c0c3aaea7d2a10984
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-09-30 14:16:10 -0500

    Sort columns

--- sql/sub/columns.sql
+++ sql/sub/columns.sql
@@ -1,3 +1,4 @@
 SELECT *
-FROM   information_schema.columns order by table_name
+FROM   information_schema.columns 
+order by table_catalog ASC, table_schema ASC, table_name ASC, column_name ASC
 \g (format=html)
