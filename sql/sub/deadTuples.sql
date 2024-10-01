04883e05ca8ba0836070e6da903331b4e15cea1b
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-10-01 10:26:19 -0500

    Update deadTuples.sql

--- sql/sub/deadTuples.sql
+++ sql/sub/deadTuples.sql
@@ -3,7 +3,7 @@ round(CAST((float4(n_dead_tup)/float4(n_live_tup + n_dead_tup))*100 as numeric),
 last_vacuum, last_autovacuum, last_analyze, last_autoanalyze,
 n_tup_ins as "inserts",n_tup_upd as "updates",n_tup_del as "deletes", 
 (n_tup_ins + n_tup_upd + n_tup_del) as "total_activity",
-FROM pg_stat_user_tables 
+FROM pg_stat_all_tables 
 WHERE (n_live_tup + n_dead_tup) > 0
 ORDER BY "dead_tuples" DESC
  \g (format=html)
