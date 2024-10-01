7478f81ba6868b16f81586ea655d2c300305c3b9
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-10-01 08:22:51 -0500

    Cumulative connections

--- sql/sub/connections.sql
+++ sql/sub/connections.sql
@@ -5,6 +5,12 @@ SELECT setting AS max_connections FROM pg_settings
 WHERE name = 'max_connections'
 \g (format=html)
 
+select datname, usename, client_addr, count(*) 
+from pg_stat_activity 
+group by datname, usename, client_addr 
+order by count(*) desc
+\g (format=html)
+
 SELECT * FROM pg_stat_activity
 ORDER BY query_start ASC
 \g (format=html)
