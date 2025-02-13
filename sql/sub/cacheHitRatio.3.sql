06f9243e6c7f3259162bae13b8c7889ddbab41e2
jspilinek <101371702+jspilinek@users.noreply.github.com>
2025-02-13 13:56:22 -0600

    Fixed division by zero

--- sql/sub/cacheHitRatio.3.sql
+++ sql/sub/cacheHitRatio.3.sql
@@ -3,13 +3,20 @@
 SELECT 'user tables' as source,
   sum(toast_blks_read) as toast_blks_read,
   sum(toast_blks_hit)  as toast_blks_hit,
-  ROUND(sum(toast_blks_hit) / (sum(toast_blks_hit) + sum(toast_blks_read)) * 100,2) as "toast cache hit ratio"
+  CASE 
+    WHEN (sum(toast_blks_hit) + sum(toast_blks_read)) = 0 THEN 0
+    ELSE ROUND(sum(toast_blks_hit) / (sum(toast_blks_hit) + sum(toast_blks_read)) * 100, 2)
+  END as "toast cache hit ratio"
 FROM 
   pg_statio_user_tables
-UNION SELECT 'all tables' as source,
+UNION 
+SELECT 'all tables' as source,
   sum(toast_blks_read) as toast_blks_read,
   sum(toast_blks_hit)  as toast_blks_hit,
-  ROUND(sum(toast_blks_hit) / (sum(toast_blks_hit) + sum(toast_blks_read)) * 100,2) as "toast cache hit ratio"
+  CASE 
+    WHEN (sum(toast_blks_hit) + sum(toast_blks_read)) = 0 THEN 0
+    ELSE ROUND(sum(toast_blks_hit) / (sum(toast_blks_hit) + sum(toast_blks_read)) * 100, 2)
+  END as "toast cache hit ratio"
 FROM 
   pg_statio_all_tables;
 \pset format aligned
--- sql/sub/cacheHitRatio.4.sql
+++ sql/sub/cacheHitRatio.4.sql
@@ -3,13 +3,20 @@
 SELECT 'user tables' as source,
   sum(tidx_blks_read) as tidx_blks_read,
   sum(tidx_blks_hit)  as tidx_blks_hit,
-  ROUND(sum(tidx_blks_hit) / (sum(tidx_blks_hit) + sum(tidx_blks_read)) * 100,2) as "tidx cache hit ratio"
+  CASE 
+    WHEN (sum(tidx_blks_hit) + sum(tidx_blks_read)) = 0 THEN 0
+    ELSE ROUND(sum(tidx_blks_hit) / (sum(tidx_blks_hit) + sum(tidx_blks_read)) * 100, 2)
+  END as "tidx cache hit ratio"
 FROM 
   pg_statio_user_tables
-UNION SELECT 'all tables' as source,
+UNION 
+SELECT 'all tables' as source,
   sum(tidx_blks_read) as tidx_blks_read,
   sum(tidx_blks_hit)  as tidx_blks_hit,
-  ROUND(sum(tidx_blks_hit) / (sum(tidx_blks_hit) + sum(tidx_blks_read)) * 100,2) as "tidx cache hit ratio"
+  CASE 
+    WHEN (sum(tidx_blks_hit) + sum(tidx_blks_read)) = 0 THEN 0
+    ELSE ROUND(sum(tidx_blks_hit) / (sum(tidx_blks_hit) + sum(tidx_blks_read)) * 100, 2)
+  END as "tidx cache hit ratio"
 FROM 
   pg_statio_all_tables;
 \pset format aligned
