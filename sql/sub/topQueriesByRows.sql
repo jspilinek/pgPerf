34fd3c0f21dd036d83f1af08890487477c6bf1df
jspilinek <101371702+jspilinek@users.noreply.github.com>
2025-02-24 07:49:12 -0600

    Fixed division by zero

--- sql/sub/topQueriesByRows.sql
+++ sql/sub/topQueriesByRows.sql
@@ -1,22 +1,22 @@
 \pset format html
---SQL statements returning the most rows
+-- SQL statements returning the most rows
 SELECT roles.rolname AS "user", 
   db.datname AS "database", 
   ss.queryid,
   ss.calls,
   ss.rows,
-  ss.rows / ss.calls AS "Rows per Call",
+  CASE WHEN ss.calls > 0 THEN ss.rows / ss.calls ELSE 0 END AS "Rows per Call",
   substring(replace(replace(replace(ss.query,E'\n',''),E'\r',''),E'\t','') FROM 1 FOR 64) AS "query substring 64"
 FROM pg_stat_statements ss
 JOIN pg_roles roles ON roles.oid = ss.userid
 JOIN pg_database db ON db.oid = ss.dbid
 ORDER BY "Rows per Call" DESC
-limit 50;
+LIMIT 50;
 
---Full output
-SELECT ss.rows / ss.calls AS "Rows per Call",
+-- Full output
+SELECT CASE WHEN ss.calls > 0 THEN ss.rows / ss.calls ELSE 0 END AS "Rows per Call",
 *
-FROM  pg_stat_statements ss
+FROM pg_stat_statements ss
 ORDER BY "Rows per Call" DESC
-limit 50;
+LIMIT 50;
 \pset format aligned
