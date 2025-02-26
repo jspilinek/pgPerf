cf59ee6833d91d37544232da078e4b6b8c0f5920
jspilinek <101371702+jspilinek@users.noreply.github.com>
2025-02-26 10:05:05 -0600

    Fixed division by zero

--- sql/topQueriesByRows.sql
+++ sql/topQueriesByRows.sql
@@ -36,7 +36,7 @@ SELECT roles.rolname AS "user",
   ss.queryid,
   ss.calls,
   ss.rows,
-  ss.rows / ss.calls AS "Rows per Call",
+  CASE WHEN ss.calls > 0 THEN ss.rows / ss.calls ELSE 0 END AS "Rows per Call",
   substring(replace(replace(replace(ss.query,E'\n',''),E'\r',''),E'\t','') FROM 1 FOR 64) AS "query substring 64"
 FROM pg_stat_statements ss
 JOIN pg_roles roles ON roles.oid = ss.userid
@@ -46,7 +46,7 @@ limit 50
 ;
 
 --Full output
-SELECT ss.rows / ss.calls AS "Rows per Call",
+SELECT CASE WHEN ss.calls > 0 THEN ss.rows / ss.calls ELSE 0 END AS "Rows per Call",
 *
 FROM  pg_stat_statements ss
 ORDER BY "Rows per Call" DESC
